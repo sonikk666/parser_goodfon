273d4f927377022509bf731e47f700cf767dfa37
Никита Михайлов <sonikk666@yandex.ru>
2023-02-27 09:20:42 +0300

    Added page parsing

--- scraper_goodfon.py
+++ scraper_goodfon.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LOGINURL = 'https://www.goodfon.ru/auth/signin/'
-DATAURL = 'https://www.goodfon.ru/search/?q=cars'  # default value
+DATAURL = 'https://www.goodfon.ru/search/?q=cars&page=1'  # default value
 GOODFON_URL = os.getenv('GOODFON_URL')
 
 if GOODFON_URL:
@@ -15,6 +15,8 @@ if GOODFON_URL:
 
 USERNAME = os.getenv('GOODFON_USERNAME')
 PASSWORD = os.getenv('GOODFON_PASSWORD')
+
+photos_urls = []
 
 
 def authorization():
@@ -27,45 +29,68 @@ def authorization():
     formdata = {
         'login': USERNAME,
         'password': PASSWORD,
-        'loginButton' : 'Войти',
+        'loginButton': 'Войти',
     }
 
     # Authenticate
-    session.post(LOGINURL, data=formdata, headers=req_headers, allow_redirects=False)
+    session.post(
+        LOGINURL, data=formdata,
+        headers=req_headers, allow_redirects=False
+    )
 
     return session
 
 
 def scraper(session, flag='preview'):
     response = session.get(DATAURL)
-    print(response.status_code)
-    
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    walpapers = soup.find_all('div', class_='wallpapers__item')
+    pagination_info = soup.find('div', class_='paginator__page').text
+    count_pages = pagination_info.replace(
+        ' ', ''
+    ).replace('1из', '').replace('\n', '')
+    print(int(count_pages))
 
-    photos_urls = []
+    for page in range(1, int(count_pages)):
+        print(page)
+        url = DATAURL.replace('page=1', f'page={page}')
+        # print(response.status_code)
+        # print(url)
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    n = 0
-    for walpaper in walpapers:
-        n += 1
-        img = walpaper.find('img', class_='wallpapers__item__img').get('src')
-        size = walpaper.find('div', class_='wallpapers__item__bottom').find('small').text
+        walpapers = soup.find_all('div', class_='wallpapers__item')
 
-        link_download_full_img = img.replace('wallpaper/big', f'original/{size}')
-        link_download_preview_img = img.replace('big', 'nbig')
-        # print(f'Ссылка №{n}: {link_download_preview_img}')
+        n = 0
+        for walpaper in walpapers:
+            n += 1
+            img = walpaper.find(
+                'img', class_='wallpapers__item__img'
+            ).get('src')
+            size = walpaper.find(
+                'div', class_='wallpapers__item__bottom'
+            ).find('small').text
 
-        link = link_download_preview_img
-        if flag == 'full':
-            link = link_download_full_img
-        
-        photos_urls.append(link)
+            link_download_full_img = img.replace(
+                'wallpaper/big', f'original/{size}'
+            )
+            link_download_preview_img = img.replace('big', 'nbig')
+            # print(f'Ссылка №{n}: {link_download_preview_img}')
 
-        # count pict for test
-        # if n == 5:
-        #     break
+            link = link_download_preview_img
+            if flag == 'full':
+                link = link_download_full_img
 
-    print('------------------------------------------')
+            photos_urls.append(link)
+
+            # count pict for test
+            if n == 5:
+                break
+
+        print('------------------------------------------')
+        # count page for test
+        if page == 6:
+            break
     return photos_urls
 
 
@@ -77,7 +102,8 @@ def download_photo(session, one_urls, path):
         print('-----Saving completed-----')
         print('')
 
-def name_file(one_urls):
+
+def name_and_path_file(one_urls):
     tail, _, _ = one_urls[::-1].partition('/')
     name = tail[::-1][:-4]
 
@@ -85,7 +111,6 @@ def name_file(one_urls):
     if 'nbig' in one_urls:
         name += '_preview'
         folder = 'media/preview'
-
 
     print(f'Имя файла: {name}')
 
@@ -97,9 +122,9 @@ def name_file(one_urls):
 def checking_and_download_call(photos_urls):
 
     for one_urls in photos_urls:
-        path = name_file(one_urls)
+        path = name_and_path_file(one_urls)
         print(path)
-        
+
         if not os.path.isfile(path):
             print('-----Файла нет => начинаю загрузку-----')
             download_photo(session, one_urls, path)
